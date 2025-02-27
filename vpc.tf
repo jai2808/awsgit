f9b2fd093d0b227f42aff4cbef40160ded27d28c
jai2808 <JAI2808@GMAIL.COM>
2025-02-28 00:03:37 +0530

    subnet added

--- vpc.tf
+++ vpc.tf
@@ -6,3 +6,13 @@ resource "aws_vpc" "vpc1" {
     Name = "rhel8-vpc1"
   }
 }
+
+resource "aws_subnet" "sub1" {
+  vpc_id = aws_vpc.vpc1.id
+  cidr_block = "10.0.1.0/24"
+  
+  tags = {
+    Name = "rhel8-sub1"
+  }
+
+}
