615a00125eeb3857605cedb642def97b70fd5163
jai2808 <JAI2808@GMAIL.COM>
2025-02-28 12:39:01 +0530

    rewtie

--- main.tf
+++ main.tf
@@ -1,22 +1,11 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
-  required_version = ">= 1.2.0"
-}
-
 provider "aws" {
-  region  = "us-west-2"
+    region = "us-east-2"  
 }
 
-resource "aws_instance" "public_instance" {
- ami           = var.ami
- instance_type = var.instance_type
-
- tags = {
-   Name = var.name_tag,
- }
+resource "aws_instance" "foo" {
+  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  instance_type = "t2.micro"
+  tags = {
+      Name = "TF-Instance"
+  }
 }
