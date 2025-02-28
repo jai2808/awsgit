613ca4b84ec30cc4eb6b951becdcdcb11441448d
jai2808 <JAI2808@GMAIL.COM>
2025-02-28 12:30:17 +0530

    test

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
 resource "aws_instance" "public_instance" {
  ami           = var.ami
  instance_type = var.instance_type
