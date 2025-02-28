705577dbfb46dd97cedf1b5e0b448c7d5f3f3ef9
jai2808 <JAI2808@GMAIL.COM>
2025-02-28 13:15:28 +0530

    subnet

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "foo" {
   ami           = "ami-0041f866474ace6ea" # us-west-2
   instance_type = "t2.micro"
+  subnet_id = "subnet-0704c036c03980b2d"
   tags = {
       Name = "TF-Instance"
   }
