138779cf713104428f76d1b1c21c15d1aae05db1
jai2808 <JAI2808@GMAIL.COM>
2025-03-01 16:06:52 +0530

    fix

--- ec2.tf
+++ ec2.tf
@@ -3,7 +3,6 @@ resource "aws_instance" "foo" {
   ami                         = var.ami # us-west-2
   instance_type               = var.instancetype
   subnet_id                   = var.subnetid
-  count                       = 1
   associate_public_ip_address = true
   key_name                    = var.keyname
   security_groups             = ["sg-06702b76159fecc7d"]
