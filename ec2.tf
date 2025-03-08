9076cc0de9ada8b2f3af6c31130f3ab9db235fdc
jai2808 <JAI2808@GMAIL.COM>
2025-03-08 13:01:07 +0530

    full

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,77 @@
+resource "aws_vpc" "rhel8-vpc" {
+  cidr_block = "10.0.0.0/24"
+  tags = {
+    Name = "rhel8-vpc"
+  }
+}
+
+resource "aws_subnet" "rhel8-subnet" {
+  vpc_id     = aws_vpc.rhel8-vpc.id
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "rhel8-subnet"
+  }
+}
+
+
+resource "aws_internet_gateway" "rhel8-rt" {
+  vpc_id = aws_vpc.rhel8-vpc.id
+  tags = {
+    Name = "rhel8-igw"
+  }
+}
+
+resource "aws_route_table" "rhel8-rt" {
+  vpc_id = aws_vpc.rhel8-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.rhel8-rt.id
+  }
+
+  route {
+    cidr_block = "10.0.0.0/24"
+    gateway_id = "local"
+  }
+
+  tags = {
+    name = "rhel8-rt"
+  }
+}
+
+resource "aws_route_table_association" "rt_asso" {
+  subnet_id      = aws_subnet.rhel8-subnet.id
+  route_table_id = aws_route_table.rhel8-rt.id
+}
+
+resource "aws_network_acl" "rhel_acl" {
+  vpc_id = aws_vpc.rhel8-vpc.id
+  tags = {
+    Name = "rhel_acl"
+  }
+}
+
+resource "aws_network_acl_rule" "allow_all_inbound" {
+  rule_number    = 100
+  egress         = false
+  protocol       = "-1"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  network_acl_id = aws_network_acl.rhel_acl.id
+}
+
+
+resource "aws_network_acl_rule" "allow_all_outboud" {
+  rule_number    = 100
+  egress         = true
+  protocol       = "-1"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  network_acl_id = aws_network_acl.rhel_acl.id
+}
+
+
 resource "aws_instance" "foo" {
 
   ami                         = var.ami # us-west-2
