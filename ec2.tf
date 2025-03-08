871bc8633cbb7a4b4474132a9c0f8f9e19acb3f7
jai2808 <JAI2808@GMAIL.COM>
2025-03-08 15:01:51 +0530

    fix

--- ec2.tf
+++ ec2.tf
@@ -83,42 +83,6 @@ resource "aws_security_group" "rhel_sg" {
   }
 }
 
-resource "aws_security_group_rule" "ssh" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.rhel_sg.id
-}
-
-resource "aws_security_group_rule" "http" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.rhel_sg.id
-}
-
-resource "aws_security_group_rule" "jenkins" {
-  type              = "ingress"
-  from_port         = 8080
-  to_port           = 8080
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.rhel_sg.id
-}
-
-resource "aws_security_group_rule" "egressall" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.rhel_sg.id
-}
-
 
 resource "aws_instance" "foo" {
 
