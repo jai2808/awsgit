c67c1e3f80cf4ef246e5a3bd75577f5bdab0b137
jai2808 <JAI2808@GMAIL.COM>
2025-03-08 16:00:45 +0530

    ip

--- variables.tf
+++ variables.tf
@@ -1,23 +1,27 @@
 variable "instance_name" {
-    description = "This is default instance name"
-    type = string
-    default = "rhel81"
+  description = "This is default instance name"
+  type        = string
+  default     = "rhel81"
 }
 
 variable "ami" {
-    description = "This is the AMI Image"
-    type = string
-    default = "ami-002acc74c401fa86b"
+  description = "This is the AMI Image"
+  type        = string
+  default     = "ami-002acc74c401fa86b"
 }
 
 variable "instancetype" {
-    description = "This is the AMI Instance type"
-    type = string
-    default = "t2.micro"
+  description = "This is the AMI Instance type"
+  type        = string
+  default     = "t2.micro"
 }
 
 variable "keyname" {
-    description = "This is the Key pair name"
-    type = string
-    default = "oralogin"
+  description = "This is the Key pair name"
+  type        = string
+  default     = "oralogin"
+}
+
+output "EC2IP" {
+  value = aws_instance.foo.public_ip
 }
